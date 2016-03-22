2673706ba20387705b6083aba67a8545ea7aedc6
zqbinggong <2420086330@qq.com>
2016-03-22 17:57:32 +0800

    Update chapter1.py

--- chapter1.py
+++ chapter1.py
@@ -1,3 +1,5 @@
+## programming to solve fall-with-resistance model and provide a diagrammatic analysis
+# written by zhangqinag, last modified on 2016/03/22
 from pylab import *
 import pickle
 
@@ -8,6 +10,7 @@ b = 0
 dt = 0
 n = 0
 
+#receive input paradise and calculate a suitable end time
 def initialize(v, t, _a, _b, _dt, _n):
     global a,b, dt, n
     v.append(float(raw_input("initial speed -> ")))
@@ -19,6 +22,7 @@ def initialize(v, t, _a, _b, _dt, _n):
     n = int(time/dt)
     return 0
 
+## calculate the v and t every moment
 def calculate(v, t, a, b, dt, n):
     print v
     print a
@@ -31,6 +35,7 @@ def calculate(v, t, a, b, dt, n):
         t.append(t[i - 1] + dt)
     return 0
 
+#save the results
 def store(v, t, n):
     mfile = open("notes.txt", "a")
     for i in range(n):
@@ -54,11 +59,13 @@ initialize(v, t, a, b, dt ,n)
 calculate(v, t, a, b, dt, n)
 store(v, t, n)
 
+#draw the asymptote
 ymin=min(v)
 ymax=max(v)
 v_t= ymin
 plot([0,ymax],[v_t,v_t],label='the terminal speed', color='red', linewidth=2.5, linestyle="--")
 
+#adjust the diagram
 xmin, xmax = min(t), max(t)
 ymin, ymax = int(min(v)), round(max(v))
 
@@ -75,9 +82,11 @@ ax.spines['bottom'].set_position(('data',0))
 ax.yaxis.set_ticks_position('left')
 ax.spines['left'].set_position(('data',0))
 
+#name the label
 ylabel('the spped v')
 xlabel('the time t')
 
+#draw the diagram
 plot(t, v, label='free fall with resistance')
 legend(loc='upper center')
 show()
